90f441ea562eaee4200b93b9db015911d0a836fe
HoussineCID <108723984+HoussineCID@users.noreply.github.com>
2023-09-05 15:11:21 +0100

    add component page-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent
   ],
   imports: [
     BrowserModule,
