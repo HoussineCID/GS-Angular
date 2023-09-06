944674903268ef9d9032874a42c2001875addcf3
HoussineCID <108723984+HoussineCID@users.noreply.github.com>
2023-09-06 11:50:12 +0100

    Implementation de  page-inscription

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { PageLoginComponent } from './pages/page-login/page-login.component';
     NgbModule,
     BrowserAnimationsModule,
     PageLoginComponent,
+    PageInscriptionComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/page-inscription/page-inscription.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-page-inscription',
+  templateUrl: './page-inscription.component.html',
+  styleUrls: ['./page-inscription.component.scss'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
+})
+export class PageInscriptionComponent {}
